f38421854b503ef3de123a4e98f467ca544ab0fe
Braulio Mora <brauliocdc59@gmail.com>
2022-01-19 15:52:08 -0800

    created a server.js file defining express app using handlebars temp engine, middleware and connection to mysql db

---
+++ server.js
@@ -0,0 +1,42 @@
+//Definitions
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//app set up
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Set up Handlebars engine and include custom helpers
+const hbs = exphbs.create({helpers});
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false, 
+  saveUninitialized: true, 
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+//setting what template engine express should use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+//Sync Db connection with Controller/API layer
+sequelize.sync({force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+})
