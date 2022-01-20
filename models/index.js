a6480566f0ddbf98ed298b525eaea3e969701b88
Braulio Mora <brauliocdc59@gmail.com>
2022-01-20 00:44:25 -0800

    created models for users, posts and comments

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'author_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'author_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'author_id',
+    onDelete: 'CASCADE'
+  });
+  
+Comment.belongsTo(User, {
+foreignKey: 'author'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+  });
+  
+Comment.belongsTo(Post, {
+foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Comment};
